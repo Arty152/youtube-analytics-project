efeb383ba214e61a11688e8cb8a479ff318dca8c
Andrey Zhirkov <zhirkov.arty@gmail.com>
2023-08-10 22:28:54 +0300

    Задание 2 выполнено

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 import os
 import json
 from googleapiclient.discovery import build
@@ -7,14 +6,34 @@ from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    attribute_values = []
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        api_key: str = os.getenv('API_KEY')
-        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = self.channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriber_сount = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_сount = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pprint(json.dumps(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()))
+        print(f'Информация о канале:\n{self.channel}')
 
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API"""
+        return cls.youtube
+
+    def to_json(self, data: str) -> None:
+        """Сохраняет в файл значения атрибутов экземпляра `Channel`"""
+        self.attribute_values.append((self.title, self.description, self.url,
+                                      self.subscriber_сount, self.video_count, self.view_сount))
+
+        with open("moscowpython.json", "w") as f:
+            json.dump(Channel.attribute_values, f)
